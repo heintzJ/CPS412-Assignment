6e3294b750907c9e828f5ef91d435178a9f1dcc2
heintzJ <96451646+heintzJ@users.noreply.github.com>
2023-03-23 14:27:30 -0400

    Added chatgptUsage graph

--- CPS412 Assignment/graphs.py
+++ CPS412 Assignment/graphs.py
@@ -64,5 +64,47 @@ def facultyAndEducation():
   plt.legend(['Yes', 'No'], frameon=False)
   plt.show()
 
+# Faculty vs ChatGPT usage
+def chatgptUsage():
+  x = []
+  yes = []
+  no_will = []
+  no_willnot = []
+  with open('survey.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    # skip first line
+    next(plots)
+    faculties = {"Computer Science":[0, 0, 0],
+                 "Media":[0, 0, 0],
+                 "Humanities":[0, 0, 0],
+                 "Engineering":[0, 0, 0],
+                 "Health/Life Sciences":[0, 0, 0],
+                 "Business":[0, 0, 0]}
+    for row in plots:
+        if row[3] in faculties.keys():
+          # If the key exists, increment its value by 1
+          if row[6] == "Yes":
+            faculties[row[3]][0] += 1
+          elif row[6] == "No, but I am planning to":
+            faculties[row[3]][1] += 1
+          elif row[6] == "No, and I am not planning to":
+            faculties[row[3]][2] += 1
+    for k in faculties:
+      x.append(k)
+      yes.append(faculties[k][0])
+      no_will.append(faculties[k][1])
+      no_willnot.append(faculties[k][2])
+
+  plt.figure(figsize=(10,6))
+  plt.bar(x, yes, color='b', width=0.72, label="Yes")
+  plt.bar(x, no_will, color='orange', width=0.72, label='No, but I am planning on it')
+  plt.bar(x, no_willnot, color='red', width=0.72, label='No, and I am not planning on it')
+  plt.xlabel('Faculty')
+  plt.ylabel('Student Responses')
+  plt.title('Have you Used ChatGPT?')
+  plt.legend()
+  plt.show()
+
 # graphOfAges()
-facultyAndEducation()
+# facultyAndEducation()
+chatgptUsage()
